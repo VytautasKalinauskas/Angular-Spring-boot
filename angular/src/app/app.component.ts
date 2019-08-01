3f887415d95188f2623b9c6d2c6fb850ef9a8582
Vytautas Kalinauskas <vyckalinauskas@gmail.com>
2019-08-01 22:01:45 +0300

    Initial front end commit
    Creating front-end angular project

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //template: '<h1> vycka </h1>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ang';
+  message = 'priviet kentam huj mentam';
+}
